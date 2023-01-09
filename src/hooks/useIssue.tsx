78132bd3bf5c75ee14a446c47b61563dd6e370c4
Daniel Mojica <danielrmojicac@gmail.com>
2023-01-09 15:15:02 -0300

    comments added

--- src/hooks/useIssue.tsx
+++ src/hooks/useIssue.tsx
@@ -1,9 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import { Issue } from '../issues/interfaces';
 import { githubApi } from '../api/githubApi';
+import { sleep } from '../helpers/sleep';
 
 const getIssueInfo = async (issueNumber: number): Promise<Issue> => {
+  await sleep(2);
   const { data } = await githubApi.get<Issue>(`/issues/${issueNumber}`);
+  return data;
+};
+
+const getIssueComments = async (issueNumber: number): Promise<Issue[]> => {
+  await sleep(2);
+  const { data } = await githubApi.get<Issue[]>(
+    `/issues/${issueNumber}/comments`
+  );
   return data;
 };
 
@@ -11,7 +21,15 @@ export const useIssue = (issueNumber: number) => {
   const issueQuery = useQuery(['issue', issueNumber], () =>
     getIssueInfo(issueNumber)
   );
+  const issueCommentsQuery = useQuery(
+    ['issue', issueNumber, 'comments'],
+    () => getIssueComments(issueQuery.data!.number),
+    {
+      enabled: issueQuery.data !== undefined,
+    }
+  );
   return {
     issueQuery,
+    issueCommentsQuery,
   };
 };
